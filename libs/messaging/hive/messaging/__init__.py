e40eea1bdc8422d967d36801cfbd993987feed73
Gary Benson <gary@gbenson.net>
2024-09-13 00:43:08 +0100

    hive-messaging: Smoke test

--- libs/messaging/hive/messaging/__init__.py
+++ libs/messaging/hive/messaging/__init__.py
@@ -4,4 +4,5 @@ from .message_bus import MessageBus
 
 DEFAULT_MESSAGE_BUS = MessageBus()
 
+blocking_connection = DEFAULT_MESSAGE_BUS.blocking_connection
 send_to_queue = DEFAULT_MESSAGE_BUS.send_to_queue

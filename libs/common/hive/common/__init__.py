0d1a7f2680aa609cb044b16273f029550a5ac343
Gary Benson <gary@gbenson.net>
2024-10-15 22:29:43 +0100

    hive-common: Incorporate hive-config

--- libs/common/hive/common/__init__.py
+++ libs/common/hive/common/__init__.py
@@ -1,1 +1,2 @@
 from .argument_parser import HiveArgumentParser as ArgumentParser
+from .config import read as read_config
--- libs/common/hive/common/testing.py
+++ libs/common/hive/common/testing.py
@@ -1,7 +1,11 @@
+import pytest
+
 try:
     from pep440_version_utils import Version
-except ImportError:
+except ImportError:  # pragma: no cover
     pass
+
+from .config import DEFAULT_READER
 
 
 def assert_is_valid_version(version: str):
@@ -9,6 +13,14 @@ def assert_is_valid_version(version: str):
         assert str(Version(version)) == version
     except Exception as e:
         raise AssertionError(f"Invalid version {version!r}") from e
+
+
+@pytest.fixture
+def test_config_dir(tmp_path, monkeypatch):
+    dirname = str(tmp_path)
+    with monkeypatch.context() as m:
+        m.setattr(DEFAULT_READER, "search_path", [dirname])
+        yield dirname
 
 
 def want_to_see(caplog, msg):

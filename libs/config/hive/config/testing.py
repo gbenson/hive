45726725374802aa1f64ec2fd748f50309584eb5
Gary Benson <gary@gbenson.net>
2024-10-15 22:48:42 +0100

    hive-config: Move everything into hive-common

--- libs/config/hive/config/testing.py
+++ libs/config/hive/config/testing.py
@@ -1,11 +1,1 @@
-import pytest
-
-from . import DEFAULT_READER
-
-
-@pytest.fixture
-def test_config_dir(tmp_path, monkeypatch):
-    dirname = str(tmp_path)
-    with monkeypatch.context() as m:
-        m.setattr(DEFAULT_READER, "search_path", [dirname])
-        yield dirname
+from hive.common.testing import test_config_dir  # noqa: F401

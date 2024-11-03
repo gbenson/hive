bc65bc5ba1d0a0e116e3655f63da884d3494aebb
Gary Benson <gary@gbenson.net>
2024-11-03 02:50:12 +0000

    hive-common: Add hive.common.openai

--- libs/email/hive/email/optional/openai.py
+++ libs/email/hive/email/optional/openai.py
@@ -1,23 +1,13 @@
-from hive.common import read_config
-
 try:
-    from openai import (
+    from hive.common.openai import (
         APIStatusError,
-        OpenAI as _OpenAI,
+        OpenAI,
     )
 
 except ModuleNotFoundError:
     class APIStatusError(Exception):
         pass
 
-    class _OpenAI:
+    class OpenAI:
         def __init__(self, *args, **kwargs):
             raise ModuleNotFoundError("hive-email[openai] not installed")
-
-
-class OpenAI(_OpenAI):
-    def __init__(self, **kwargs):
-        api_key = kwargs.pop("api_key", None)
-        if not api_key:
-            api_key = read_config("openai")["api_key"]
-        super().__init__(api_key=api_key, **kwargs)

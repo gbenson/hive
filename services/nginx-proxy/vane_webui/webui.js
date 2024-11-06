a3e919b0f0fedb556bec2c5bbda2c17f99fb0831
Gary Benson <gary@gbenson.net>
2024-11-06 21:23:13 +0000

    vane-webui: Wrap everything in a class

--- services/nginx-proxy/vane_webui/webui.js
+++ services/nginx-proxy/vane_webui/webui.js
@@ -1,153 +1,162 @@
-function fatalError(msg) {
-  const main = document.getElementById("output");
-  main.innerText = msg;
-}
-
-function httpError(response) {
-  fatalError(`${response.status} ${response.statusText}`);
-}
-
 document.addEventListener("DOMContentLoaded", () => {
-  getSession();
+  const app = new WebUI();
+  app.run();
 });
 
-async function getSession() {
-  const response = await fetch("api/login");
-  if (response.status == 204) {
-    return gotSession();
-  }
-  if (response.status != 200) {
-    return httpError(response);
+class WebUI {
+  outputArea = document.getElementById("output");
+
+  run() {
+    this.getSession();
   }
 
-  const csrf = document.getElementById("xov");
-
-  const json = await response.json();
-  csrf.value = json.csrf;
-
-  const loginForm = document.getElementById("login");
-  const user = document.getElementById("un");
-
-  loginForm.addEventListener("submit", async (event) => {
-    event.preventDefault();
-
-    const pass = document.getElementById("pw");
-
-    const response = await fetch("api/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        user: user.value,
-        pass: pass.value,
-        csrf: csrf.value,
-      }),
-    });
-
+  async getSession() {
+    const response = await fetch("api/login");
     if (response.status == 204) {
-      loginForm.style.display = "none";
-      return gotSession();
+      return this.gotSession();
     }
     if (response.status != 200) {
-      return httpError(response);
+      return this.httpError(response);
     }
 
-    const button = loginForm.getElementsByTagName("button")[0];
-    button.style.backgroundColor = "red";
+    const csrf = document.getElementById("xov");
 
     const json = await response.json();
     csrf.value = json.csrf;
 
+    const loginForm = document.getElementById("login");
+    const user = document.getElementById("un");
+
+    loginForm.addEventListener("submit", async (event) => {
+      event.preventDefault();
+
+      const pass = document.getElementById("pw");
+
+      const response = await fetch("api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user: user.value,
+          pass: pass.value,
+          csrf: csrf.value,
+        }),
+      });
+
+      if (response.status == 204) {
+        loginForm.style.display = "none";
+        return this.gotSession();
+      }
+      if (response.status != 200) {
+        return this.httpError(response);
+      }
+
+      const button = loginForm.getElementsByTagName("button")[0];
+      button.style.backgroundColor = "red";
+
+      const json = await response.json();
+      csrf.value = json.csrf;
+
+      user.focus();
+    }, false);
+
+    loginForm.style.display = "";
     user.focus();
-  }, false);
-
-  loginForm.style.display = "";
-  user.focus();
-}
-
-function gotSession() {
-  const outputArea = document.getElementById("output");
-  const sse = new EventSource("api/events");
-
-  sse.addEventListener("error", (event) => {
-    fatalError("Event source didn't open😞");
-  });
-  sse.addEventListener("open", (event) => {
-    gotEventSource(outputArea);
-  });
-  sse.addEventListener("message", (event) => {
-    gotMessages(JSON.parse(event.data), outputArea);
-  });
-}
-
-function gotEventSource(outputArea) {
-  const form = document.getElementById("chat");
-  const input = document.getElementById("input");
-
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    let userInput = input.value;
-    input.value = "";
-    processInput(userInput, outputArea);
-  }, false);
-
-  const footer = document.getElementById("footer");
-  footer.style.display = "";
-  document.title = "H I V E";
-  input.focus();
-}
-
-function gotMessages(messages, outputArea) {
-  for (let message of messages) {
-    addToChat(outputArea, message["sender"], message["text"], true);
   }
-}
 
-const apiEndpointURL = ("https://nrtt8bz8be.execute-api.us" +
-                        "-east-1.amazonaws.com/prod/niall2");
+  gotSession() {
+    const sse = new EventSource("api/events");
 
-async function processInput(userInput, messages) {
-  userInput = userInput.trim();
-  addToChat(messages, "user", userInput);
+    sse.addEventListener("error", (event) => {
+      this.fatalError("Event source didn't open😞");
+    });
 
-  const hiveDiv = addToChat(messages, "hive", "...");
+    sse.addEventListener("open", (event) => {
+      this.gotEventSource();
+    });
 
-  const response = await fetch(apiEndpointURL, {
-    method: "POST",
-    headers: {
-      // "application/json" would be better here, but
-      // then CORS preflights every single request :(
-      "Content-Type": "text/plain",
-    },
-    body: JSON.stringify({
-      user_input: userInput,
-    }),
-  });
-  const json = await response.json();
-
-  hiveDiv.innerText = json["niall_output"];
-  hiveDiv.classList.remove("waiting");
-}
-
-function addToChat(messages, sender, message, noWait = false) {
-  const div = document.createElement("div");
-  div.classList.add("message");
-  div.classList.add(`from-${sender}`);
-
-  const div2 = document.createElement("div");
-  div2.classList.add("avatar");
-  div.appendChild(div2);
-
-  const div3 = document.createElement("div");
-  if (!noWait && sender == "hive") {
-    div3.classList.add("waiting");
+    sse.addEventListener("message", (event) => {
+      for (let message of JSON.parse(event.data)) {
+        this.addToChat(message["sender"], message["text"]);
+      }
+    });
   }
-  div3.innerText = message;
-  div.appendChild(div3);
 
-  messages.appendChild(div);
-  messages.scrollTop = messages.scrollHeight - messages.clientHeight;
-  return div3;
+  gotEventSource() {
+    const form = document.getElementById("chat");
+    const input = document.getElementById("input");
+
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      let userInput = input.value;
+      input.value = "";
+      this.processInput(userInput);
+    }, false);
+
+    const footer = document.getElementById("footer");
+    footer.style.display = "";
+    document.title = "H I V E";
+    input.focus();
+  }
+
+  async processInput(userInput) {
+    userInput = userInput.trim();
+    this.addToChat("user", userInput);
+
+    const hiveDiv = this.addToChat("hive", "...");
+    hiveDiv.classList.add("waiting");
+
+    const apiEndpointURL = ("https://nrtt8bz8be.execute-api.us" +
+                            "-east-1.amazonaws.com/prod/niall2");
+
+    const response = await fetch(apiEndpointURL, {
+      method: "POST",
+      headers: {
+        // "application/json" would be better here, but
+        // then CORS preflights every single request :(
+        "Content-Type": "text/plain",
+      },
+      body: JSON.stringify({
+        user_input: userInput,
+      }),
+    });
+    if (response.status != 200) {
+      return this.httpError(response);
+    }
+
+    const json = await response.json();
+
+    hiveDiv.innerText = json["niall_output"];
+    hiveDiv.classList.remove("waiting");
+  }
+
+  addToChat(sender, message) {
+    const div = document.createElement("div");
+    div.classList.add("message");
+    div.classList.add(`from-${sender}`);
+
+    const div2 = document.createElement("div");
+    div2.classList.add("avatar");
+    div.appendChild(div2);
+
+    const div3 = document.createElement("div");
+    div3.innerText = message;
+    div.appendChild(div3);
+
+    const output = this.outputArea;
+    output.appendChild(div);
+    output.scrollTop = output.scrollHeight - output.clientHeight;
+
+    return div3;
+  }
+
+  fatalError(message) {
+    this.addToChat("error", message);
+  }
+
+  httpError(response) {
+    this.fatalError(`${response.status} ${response.statusText}`);
+  }
 }

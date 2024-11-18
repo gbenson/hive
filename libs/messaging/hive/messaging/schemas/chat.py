59a82b6ea3696d100d8addb4896007e757a08753
Gary Benson <gary@gbenson.net>
2024-11-18 22:22:10 +0000

    hive-messaging: Add ChatMessage.from_json()

--- libs/messaging/hive/messaging/schemas/chat.py
+++ libs/messaging/hive/messaging/schemas/chat.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from dataclasses import dataclass, field, fields
 from datetime import datetime, timezone
 from typing import Any, Optional
@@ -12,6 +14,7 @@ class ChatMessage:
     timestamp: str | datetime = field(
         default_factory=lambda: datetime.now(tz=timezone.utc))
     uuid: str | UUID = field(default_factory=uuid4)
+    _unhandled: Optional[dict[str, Any]] = None
 
     def __post_init__(self):
         if not self.text and not self.html:
@@ -28,7 +31,36 @@ class ChatMessage:
         if self.uuid.version != 4:
             raise ValueError(self.uuid)
 
+    @classmethod
+    def json_keys(cls) -> list[str]:
+        names = (field.name for field in fields(cls))
+        return [name for name in names if name[0] != "_"]
+
+    @classmethod
+    def from_json(cls, message: dict[str, Any]) -> ChatMessage:
+        """Ultra-strict from-(deserialized)-JSON constructor.
+        """
+        if type(message) is not dict:
+            raise TypeError
+        if any(type(key) is not str for key in message.keys()):
+            raise TypeError
+
+        unhandled = message.copy()
+        keys = cls.json_keys()
+        values = [unhandled.pop(key, "") for key in keys]
+
+        if any(type(value) is not str for value in values):
+            raise TypeError
+
+        kwargs = dict(zip(keys, values))
+        if unhandled:
+            kwargs["_unhandled"] = unhandled
+        return cls(**kwargs)
+
+    @property
+    def has_unhandled_fields(self):
+        return bool(self._unhandled)
+
     def json(self) -> dict[str, Any]:
-        attrs = (field.name for field in fields(self))
-        items = ((attr, getattr(self, attr)) for attr in attrs)
+        items = ((key, getattr(self, key)) for key in self.json_keys())
         return dict((key, str(value)) for key, value in items if value)

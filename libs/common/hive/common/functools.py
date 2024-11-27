2b9a7009b22cdb2aea4e7c4a5421c17c1923827d
Gary Benson <gary@gbenson.net>
2024-11-27 19:55:03 +0000

    hive-common: Add hive.common.functools.chained

--- libs/common/hive/common/functools.py
+++ libs/common/hive/common/functools.py
@@ -1,4 +1,5 @@
 from itertools import count
+from typing import Callable, Optional
 
 
 def once(func):
@@ -10,3 +11,21 @@ def once(func):
             return None
         return func(*args, **kwargs)
     return wrapper
+
+
+def chained(*funcs) -> Optional[Callable]:
+    """Return a callable that's equivalent to calling each not-None
+    supplied callable in sequence with the same arguments.  If all
+    supplied callables are None then None is returned.
+    """
+    funcs = [func for func in funcs if func]
+    if not funcs:
+        return None
+    if len(funcs) == 1:
+        return funcs[0]
+
+    def result(*args, **kwargs):
+        for func in funcs:
+            func(*args, **kwargs)
+
+    return result

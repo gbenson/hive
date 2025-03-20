9f1f026faf5e8799f9738da0beb48b2537619ae9
Gary Benson <gary@gbenson.net>
2025-03-20 19:51:07 +0000

    hive-messaging: Add Message.is_json

--- libs/messaging/hive/messaging/message.py
+++ libs/messaging/hive/messaging/message.py
@@ -1,11 +1,9 @@
 import json
-import re
 
 from dataclasses import dataclass
 
+from contenttype import ContentType
 from pika.spec import Basic, BasicProperties
-
-APPLICATION_JSON_RE = re.compile(r"application/(:?.+\+)?json")
 
 
 @dataclass
@@ -22,7 +20,16 @@ class Message:
     def content_type(self) -> str:
         return self.properties.content_type
 
+    @property
+    def is_json(self) -> bool:
+        ct = ContentType.parse(self.content_type)
+        if ct.type != "application":
+            return False
+        if ct.subtype == "json":
+            return True
+        return ct.suffix == "json"
+
     def json(self):
-        if not APPLICATION_JSON_RE.fullmatch(self.content_type):
+        if not self.is_json:
             raise ValueError(self.content_type)
         return json.loads(self.body)

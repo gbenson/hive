43fc27e022f7ee52214f6c755887b7f29509e590
Gary Benson <gary@gbenson.net>
2025-04-23 23:08:41 +0100

    hive-messaging: Factor testing code out of chat-router

--- services/chat-router/tests/conftest.py
+++ services/chat-router/tests/conftest.py
@@ -1,25 +1,12 @@
-from dataclasses import dataclass, field
-
 import pytest
 
-from cloudevents.abstract import CloudEvent
-
-from hive.messaging import Channel, Message
+from hive.messaging import Channel
+from hive.messaging.testing import LogDecoder as _LogDecoder, MockChannel
 
 from hive.chat_router.pattern_graph import Matcher
 
 
-@dataclass(frozen=True)
-class ChannelCall:
-    method: str
-    routing_key: str
-    event: CloudEvent
-
-
-@dataclass(frozen=True)
-class LogDecoder:
-    call_log: list[ChannelCall]
-
+class LogDecoder(_LogDecoder):
     @property
     def send_text(self) -> str:
         send_text_calls = [
@@ -33,38 +20,9 @@ class LogDecoder:
         return call.event.data["text"]
 
 
-@dataclass(frozen=True)
-class MockChannel:
-    call_log: list[ChannelCall] = field(default_factory=list)
-
-    def __getattr__(self, attr):
-        return MockMethod(attr, self.call_log)
-
-    @property
-    def expect(self) -> LogDecoder:
-        return LogDecoder(self.call_log)
-
-
-@dataclass(frozen=True)
-class MockMethod:
-    name: str
-    _call_log: list[ChannelCall]
-
-    def __call__(self, *args, **kwargs) -> None:
-        if self.name != "basic_publish":
-            return
-        assert not args
-        assert not kwargs.pop("routing_key")
-        routing_key = kwargs.pop("exchange")
-        mandatory = kwargs.pop("mandatory", False)
-        name = "publish_request" if mandatory else "publish_event"
-        message = Message(method=None, **kwargs)
-        self._call_log.append(ChannelCall(name, routing_key, message.event()))
-
-
 @pytest.fixture
 def mock_channel():
-    return Channel(MockChannel())
+    return Channel(MockChannel(LogDecoder))
 
 
 @pytest.fixture

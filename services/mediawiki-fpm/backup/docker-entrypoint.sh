06e72b37dca3ab31501ce56d7ab4c06c46e8acaf
Gary Benson <gary@gbenson.net>
2025-05-20 23:49:09 +0100

    mediawiki-fpm: Daily backups

---
+++ services/mediawiki-fpm/backup/docker-entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+set -eu
+
+# Create a non-root user to work as.
+user=mariadb-backup
+home=/var/lib/hive/mariadb-backup
+uid=729
+gid=$uid
+
+addgroup --system --gid $gid $user
+adduser --system --uid $uid --gid $gid --home $home --shell /bin/bash --disabled-password $user
+
+# Put MariaDB credentials where mariadb-dump will find them.
+. /run/secrets/mediawiki-mariadb.env
+chown 0:0 /run/secrets
+chmod 700 /run/secrets
+
+rc=$home/.my.cnf
+touch $rc
+chmod 600 $rc
+cat >$rc  <<EOF
+[mysqldump]
+user=$MARIADB_USER
+password=$MARIADB_PASSWORD
+EOF
+unset MARIADB_USER
+unset MARIADB_PASSWORD
+chown $uid:$gid $rc
+
+export MARIADB_DATABASE
+echo 'info: Dropping privileges ...'
+exec su - $user -w MARIADB_DATABASE -c /mariadb-backup.sh

a6f9a81efc9df9dddef83250e3c380b44b2dd123
Gary Benson <gary@gbenson.net>
2025-09-25 00:18:52 +0100

    chat-router: Cache spellchecker responses

--- services/chat-router/hive/chat_router/spellchecker.py
+++ services/chat-router/hive/chat_router/spellchecker.py
@@ -1,3 +1,19 @@
-from spellchecker import SpellChecker
+from functools import lru_cache
+from typing import Optional
+
+from spellchecker import SpellChecker as _SpellChecker
+from spellchecker.utils import KeyT
+
+
+class SpellChecker(_SpellChecker):
+    def candidates(self, word: KeyT) -> Optional[set[str]]:
+        if (result := self._candidates(word)):
+            return result.copy()
+        return None
+
+    @lru_cache(maxsize=16384)
+    def _candidates(self, word: KeyT) -> Optional[set[str]]:
+        return super().candidates(word)
+
 
 spellcheck = SpellChecker()

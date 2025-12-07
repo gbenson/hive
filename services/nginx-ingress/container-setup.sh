4c4b9fe59c46b6337c1c2299672cae0914829d28
Gary Benson <gary@gbenson.net>
2025-12-07 16:20:36 +0000

    nginx-ingress: Pass substititions via secrets

--- services/nginx-ingress/container-setup.sh
+++ services/nginx-ingress/container-setup.sh
@@ -1,6 +1,15 @@
-#!/bin/sh
 chown 0:0 /run/secrets
 chmod 700 /run/secrets
+self=$(basename $f)
 for f in /run/secrets/*.htpasswd; do
   cat $f > /etc/nginx/$(basename $f)
 done
+for f in /run/secrets/*.env; do
+  entrypoint_log "$self: Sourcing $f"
+  . $f
+  for v in $(sed 's/#.*//; /^[[:space:]]*$/d; s/^\([A-Z][A-Z0-9_]*[A-Z0-9]\)=.*/\1/' $f); do
+    export $v
+  done
+done
+entrypoint_log "$self: Result:"
+env | sort | sed "s/^/ - /"
